9afd49d6f749bf8acc9b08feebd1a15b29582088
Soulslimm <61245631+soulslimm@users.noreply.github.com>
2021-10-18 00:56:32 -0400

    Update orb.js

--- orb.js
+++ orb.js
@@ -1,4 +1,4 @@
-import * as PIXI from "https://cdn.skypack.dev/pixi.js";
+import * as PIXI from "pixi.js";
 import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur";
 import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
